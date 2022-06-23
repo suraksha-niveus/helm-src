bfdcd6aa98218322977846991860e302000e314f
suraksha-niveus <103033871+suraksha-niveus@users.noreply.github.com>
2022-06-23 18:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Welcome to ArgoCD"
+    return "Welcome to CICD"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
