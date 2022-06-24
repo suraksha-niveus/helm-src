34f946d8edd8a4768e9d5323e78c1111ce05267e
suraksha-niveus <103033871+suraksha-niveus@users.noreply.github.com>
2022-06-24 20:37:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "CI"
+    return "welcome to CICD"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
